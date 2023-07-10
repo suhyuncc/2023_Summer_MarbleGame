1000255f2868b9b06ed13fa2eb7909e89b19064e
suhyuncc <69952158+suhyuncc@users.noreply.github.com>
2023-07-10 18:55:28 +0900

    Add torque

--- MarbleGame/Assets/Script/Rolling.cs
+++ MarbleGame/Assets/Script/Rolling.cs
@@ -6,24 +6,40 @@ public class Rolling : MonoBehaviour
 {
     private Rigidbody rigid;
     public float power = 1;
-    
+    private float random;
+
+
+    Coroutine coroutine;
     void Start()
     {
         rigid = GetComponent<Rigidbody>();
         rigid.isKinematic = true;
     }
 
-    
+    private void Awake()
+    {
+        //coroutine = StartCoroutine(spin());
+    }
+
+
     void Update()
     {
-        this.transform.Rotate(1, 0, 1);
         if (Input.GetKeyUp(KeyCode.Space))
         {
+            random = Random.RandomRange(50, 100);
+            //StopCoroutine(coroutine);
             rigid.isKinematic = false;
             rigid.AddForce(new Vector3(1, 0.7f, 1) * power, ForceMode.Impulse);
-            //rigid.AddForce(new Vector3(0.6f, 0.6f, 0) * power, ForceMode.Impulse);
-            
+            rigid.AddTorque(new Vector3(1, 0, 1) * random, ForceMode.Impulse);
         }
     }
 
+    IEnumerator spin()
+    {
+        while (true)
+        {
+            this.transform.Rotate(1, 0, 1);
+            yield return null;
+        }
+    }
 }
